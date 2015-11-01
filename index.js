f62c54e668fe61fd1255b20fceacae7523224864
Felipe Mendonça Marques Silva <fmmsilva@gmail.com>
2015-11-01 01:17:30 -0200

    bug fixes

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-var mapSingleObject = function (row) {
+var extend = require('util')._extend;
+
+var mapSingleObject = function (_row) {
+    var row = extend({}, _row)
     for (var key in row) {
         if (key.indexOf('.') > -1) {
             var arr = key.split('.');
@@ -12,11 +15,16 @@ var mapSingleObject = function (row) {
     return row;
 }
 
-var mapArray = function (rows) {
-    for (i in rows) {
-        var row = rows[i];
-        rows[i] = mapSingleObject(row)
+var mapArray = function (_rows) {
+    var rows = null
+    if (_rows) {
+        rows = []
     }
+    for (var i in _rows) {
+        var row = _rows[i];
+        rows.push(mapSingleObject(row))
+    }
+    return rows;
 }
 
 module.exports = function (obj) {
